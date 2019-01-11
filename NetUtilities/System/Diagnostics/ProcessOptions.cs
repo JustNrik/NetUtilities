ea0338cc2a150a53deaedc22828d5a59387c722a
Sean D. Hunt <seandhunt_7@yahoo.com>
2019-01-10 23:46:30 -0500

    Make ProcessUtilities better implemented.

---
+++ NetUtilities/System/Diagnostics/ProcessOptions.cs
@@ -0,0 +1,21 @@
+namespace System.Diagnostics
+{
+    /// <summary>
+    /// Extra options for the <see cref="Process" /> component.
+    /// </summary>
+    public class ProcessOptions
+    {
+        public ProcessOptions()
+        {
+        }
+
+        /// <summary>
+        /// Instucts the <see cref="Process" /> <keyword cref="object" /> to wait until the executed process terminates.
+        /// </summary>
+        public bool WaitForProcessExit { get; set; } = false;
+
+        /// <summary>
+        /// Gets if the target process is executing (has not been actually started yet), or is being executed (started). False if it was started.
+        public bool Executing { get; private set; } = true;
+    }
+}
