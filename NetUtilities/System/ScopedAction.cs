d7deffb626a7810eae6462e63b30f585229b4567
JustNrik <justnrik2@gmail.com>
2019-05-14 20:34:59 -0400

    added TempAction

--- NetUtilities/System/ScopedAction.cs
+++ NetUtilities/System/ScopedAction.cs
@@ -1,5 +1,5 @@
 ﻿using System.Threading.Tasks;
-#nullable enable
+
 namespace System
 {
     public readonly ref struct ScopedAction
@@ -17,7 +17,6 @@ namespace System
 
         public void Dispose()
             => _undoAction();
-
 
         public ValueTask DisposeAsync()
         {
@@ -45,10 +44,68 @@ namespace System
         public void Dispose()
             => _undoAction(_obj);
 
-
         public ValueTask DisposeAsync()
         {
             _undoAction(_obj);
+            return new ValueTask();
+        }
+    }
+
+    public readonly ref struct ScopedAction<T1, T2>
+    {
+        private readonly Action<T1, T2> _undoAction;
+        private readonly T1 _obj;
+        private readonly T2 _obj2;
+
+        public ScopedAction(T1 obj, T2 obj2, Action<T1, T2> startAction, Action<T1, T2> undoAction)
+        {
+            if (obj is null) throw new ArgumentNullException(nameof(obj));
+            if (startAction is null) throw new ArgumentNullException(nameof(startAction));
+            if (undoAction is null) throw new ArgumentNullException(nameof(undoAction));
+
+            _undoAction = undoAction;
+            _obj = obj;
+            _obj2 = obj2;
+            startAction(obj, obj2);
+        }
+
+        public void Dispose()
+            => _undoAction(_obj, _obj2);
+
+        public ValueTask DisposeAsync()
+        {
+            _undoAction(_obj, _obj2);
+            return new ValueTask();
+        }
+
+    }
+
+    public readonly ref struct ScopedAction<T1, T2, T3>
+    {
+        private readonly Action<T1, T2, T3> _undoAction;
+        private readonly T1 _obj;
+        private readonly T2 _obj2;
+        private readonly T3 _obj3;
+
+        public ScopedAction(T1 obj, T2 obj2, T3 obj3, Action<T1, T2, T3> startAction, Action<T1, T2, T3> undoAction)
+        {
+            if (obj is null) throw new ArgumentNullException(nameof(obj));
+            if (startAction is null) throw new ArgumentNullException(nameof(startAction));
+            if (undoAction is null) throw new ArgumentNullException(nameof(undoAction));
+
+            _undoAction = undoAction;
+            _obj = obj;
+            _obj2 = obj2;
+            _obj3 = obj3;
+            startAction(obj, obj2, obj3);
+        }
+
+        public void Dispose()
+            => _undoAction(_obj, _obj2, _obj3);
+
+        public ValueTask DisposeAsync()
+        {
+            _undoAction(_obj, _obj2, _obj3);
             return new ValueTask();
         }
     }
---
+++ NetUtilities/TempAction.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public readonly ref struct TempAction
+    {
+        private readonly Action _undoAction;
+        public TempAction(Action undoFunc) 
+            => _undoAction = undoFunc;
+        private static Action CreateAction<TResult>(object obj, MemberInfo memberInfo, TResult newValue)
+        {
+            TResult oldValue;
+
+            switch (memberInfo)
+            {
+                case PropertyInfo propertyInfo:
+                    oldValue = (TResult)propertyInfo.GetMethod.Invoke(obj, Array.Empty<object>());
+                    propertyInfo.SetMethod.Invoke(obj, new object[] { newValue });
+                    return () => propertyInfo.SetMethod.Invoke(obj, new object[] { oldValue });
+                case FieldInfo fieldInfo:
+                    oldValue = (TResult)fieldInfo.GetValue(obj);
+                    fieldInfo.SetValue(obj, newValue);
+                    return () => fieldInfo.SetValue(obj, oldValue);
+                default:
+                    throw new InvalidOperationException("The argument memberInfo must be a Property or a Field");
+            }
+        }
+
+        public static TempAction CreateScope<TResult>(Expression<Func<TResult>> propertyFunc, TResult newValue)
+        {
+            var memberInfo = (propertyFunc.Body as MemberExpression)?.Member;
+            var undoFunc = CreateAction(null, memberInfo, newValue);
+            return new TempAction(undoFunc);
+        }
+
+        public static TempAction CreateScope<T, TResult>(T obj, Expression<Func<T, TResult>> propertyFunc, TResult newValue)
+        {
+            var memberInfo = (propertyFunc.Body as MemberExpression)?.Member;
+            var undoFunc = CreateAction(obj, memberInfo, newValue);
+            return new TempAction(undoFunc);
+        }
+
+        public void Dispose()
+            => _undoAction();
+
+        public ValueTask DisposeAsync()
+        {
+            _undoAction();
+            return new ValueTask();
+        }
+    }
+}
