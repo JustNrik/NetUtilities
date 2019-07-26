1fcfd448795bc889959c2a3503d183475b1ffb1b
JD Davis <mxjerrett@gmail.com>
2019-07-25 22:00:41 -0500

    Started a unit testing project. (#14)

---
+++ tests/NetUtilities.Tests/Utilities/ValueStub.cs
@@ -0,0 +1,12 @@
+﻿namespace NetUtilities.Tests.Utilities
+{
+    public class ValueStub<T> 
+    {
+        public T Value { get; set; }
+        public ValueStub() { }
+        public ValueStub(T value)
+        {
+            Value = value;
+        }
+    }
+}
