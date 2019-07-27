0940dd6daf4d735435dd928dce3c36accd6c6cc5
JustNrik <justnrik2@gmail.com>
2019-07-27 16:14:35 -0400

    Added benchmark project

---
+++ NetUtilities.Benchmarks/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace NetUtilities.Benchmarks
+{
+    class Program
+    {
+        static void Main()
+        {
+            BenchmarkRunner.Run<MutableStringBench>();
+        }
+    }
+
+    public class MutableStringBench
+    {
+        [Params(10, 100, 1000, 1000)]
+        public static int Iterations;
+
+        [Benchmark(Baseline = true, Description = "String Concatenation")]
+        public void StringConcat()
+        {
+            var str = "";
+            for (var x = 0; x < Iterations; x++)
+                str += "a";
+        }
+
+        [Benchmark(Description = "MutableString Concatenation")]
+        public void MutableStringConcat()
+        {
+            var str = new MutableString("");
+            for (var x = 0; x < Iterations; x++)
+                str += "a";
+        }
+    }
+}
