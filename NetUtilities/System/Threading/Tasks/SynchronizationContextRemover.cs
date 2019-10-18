70a3a299826583c9d5d50e11c196d04ed09ed64a
JustNrik <justnrik2@gmail.com>
2019-10-18 12:18:38 -0400

    added new struct

---
+++ NetUtilities/System/Threading/Tasks/SynchronizationContextRemover.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Threading.Tasks
+{
+    /// <summary>
+    /// This structure is used to remove the current SynchronizationContext. This helps to reduce the verbosity of using <see cref="Task.ConfigureAwait(bool)"/>
+    /// </summary>
+    public struct SynchronizationContextRemover : INotifyCompletion
+    {
+        public bool IsCompleted 
+            => SynchronizationContext.Current is null;
+
+        public void OnCompleted(Action action)
+        {
+            var previous = SynchronizationContext.Current;
+
+            try
+            {
+                SynchronizationContext.SetSynchronizationContext(null);
+                action();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(previous);
+            }
+        }
+
+        public SynchronizationContextRemover GetAwaiter()
+            => this;
+
+        public void GetResult() { }
+    }
+}
