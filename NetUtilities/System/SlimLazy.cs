07176d86b1fe2430d80ea47294915b1003c7c3fc
JustNrik <justnrik@gmail.com>
2020-11-14 02:29:10 -0400

    updated SlimLazy api

--- NetUtilities/System/SlimLazy.cs
+++ NetUtilities/System/SlimLazy.cs
@@ -9,37 +9,28 @@ namespace System
     /// <typeparam name="T">
     ///     The type.
     /// </typeparam>
-    public sealed class SlimLazy<T> : ILazy<T>
+    public class SlimLazy<T> : ILazy<T>
     {
-        private T? _value;
-        private Func<T>? _valueFactory;
-        private readonly object? _lock;
+        /// <summary>
+        ///     The value.
+        /// </summary>
+        protected T? _value;
+
+        /// <summary>
+        ///     The factory delegate.
+        /// </summary>
+        protected Func<T>? _valueFactory;
 
         /// <inheritdoc/>
-        public T Value
+        public virtual T Value
         {
             get
             {
-                if (_lock is not null)
+                if (!IsInitialized)
                 {
-                    lock (_lock)
-                    {
-                        if (!IsInitialized)
-                        {
-                            _value = _valueFactory();
-                            _valueFactory = null;
-                            IsInitialized = true;
-                        }
-                    }
-                }
-                else
-                {
-                    if (!IsInitialized)
-                    {
-                        _value = _valueFactory();
-                        _valueFactory = null;
-                        IsInitialized = true;
-                    }
+                    _value = _valueFactory();
+                    _valueFactory = null;
+                    IsInitialized = true;
                 }
 
                 return _value;
@@ -49,7 +40,7 @@ namespace System
         /// <inheritdoc/>
         [MemberNotNullWhen(false, nameof(_valueFactory))]
         [MemberNotNullWhen(true, nameof(_value))]
-        public bool IsInitialized { get; private set; }
+        public virtual bool IsInitialized { get; protected set; }
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="SlimLazy{T}"/> class with the provided value.
@@ -65,23 +56,60 @@ namespace System
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="SlimLazy{T}"/> class with the provided factory delegate. 
-        ///     Optionally you can indicate if the initialization should be tread-safe.
         /// </summary>
         /// <param name="valueFactory">
         ///     The value factory delegate.
         /// </param>
-        /// <param name="threadSafe">
-        ///     Indicates if the initialization should be thread-safe.
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when <paramref name="valueFactory"/> is <see langword="null"/>.
+        /// </exception>
+        public SlimLazy(Func<T> valueFactory)
+        {
+            _valueFactory = Ensure.NotNull(valueFactory);
+        }
+    }
+
+    /// <summary>
+    ///     A thread-safe lightweight implementation of <see cref="Lazy{T}"/>, which also provides support for lazy initialization.
+    /// </summary>
+    /// <typeparam name="T">
+    ///     The type.
+    /// </typeparam>
+    public sealed class ConcurrentLazy<T> : SlimLazy<T>
+    {
+        private readonly object _lock = new();
+
+        /// <inheritdoc/>
+        public override T Value
+        {
+            get
+            {
+                lock (_lock)
+                    return base.Value;
+            }
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ConcurrentLazy{T}"/> class with the provided value.
+        /// </summary>
+        /// <param name="value">
+        ///     The value.
+        /// </param>
+        public ConcurrentLazy(T value) : base(value)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ConcurrentLazy{T}"/> class with the provided factory delegate. 
+        /// </summary>
+        /// <param name="valueFactory">
+        ///     The value factory delegate.
         /// </param>
         /// <exception cref="ArgumentNullException">
         ///     Thrown when <paramref name="valueFactory"/> is <see langword="null"/>.
         /// </exception>
-        public SlimLazy(Func<T> valueFactory, bool threadSafe = false)
+        public ConcurrentLazy(Func<T> valueFactory) : base(valueFactory)
         {
-            _valueFactory = Ensure.NotNull(valueFactory);
-
-            if (threadSafe)
-                _lock = new object();
         }
     }
 
@@ -94,38 +122,33 @@ namespace System
     /// <typeparam name="TState">
     ///     The state.
     /// </typeparam>
-    public sealed class SlimLazy<T, TState> : ILazy<T>
+    public class SlimLazy<T, TState> : ILazy<T> where TState : notnull
     {
-        private T? _value;
-        private Func<TState, T>? _valueFactory;
-        private readonly TState? _state;
-        private readonly object? _lock;
+        /// <summary>
+        ///     The value.
+        /// </summary>
+        protected T? _value;
+
+        /// <summary>
+        ///     The value factory delegate.
+        /// </summary>
+        protected Func<TState, T>? _valueFactory;
+
+        /// <summary>
+        ///     The factory delegate arguments.
+        /// </summary>
+        protected readonly TState? _state;
 
         /// <inheritdoc/>
-        public T Value
+        public virtual T Value
         {
             get
             {
-                if (_lock is not null)
+                if (!IsInitialized)
                 {
-                    lock (_lock)
-                    {
-                        if (!IsInitialized)
-                        {
-                            _value = _valueFactory(_state);
-                            _valueFactory = null;
-                            IsInitialized = true;
-                        }
-                    }
-                }
-                else
-                {
-                    if (!IsInitialized)
-                    {
-                        _value = _valueFactory(_state);
-                        _valueFactory = null;
-                        IsInitialized = true;
-                    }
+                    _value = _valueFactory(_state);
+                    _valueFactory = null;
+                    IsInitialized = true;
                 }
 
                 return _value;
@@ -136,7 +159,7 @@ namespace System
         [MemberNotNullWhen(false, nameof(_valueFactory))]
         [MemberNotNullWhen(false, nameof(_state))]
         [MemberNotNullWhen(true, nameof(_value))]
-        public bool IsInitialized { get; private set; }
+        public virtual bool IsInitialized { get; protected set; }
 
         /// <summary>
         ///     Initializes a new instance of <see cref="SlimLazy{T, TState}"/> class with the provided value.
@@ -152,7 +175,6 @@ namespace System
 
         /// <summary>
         ///     Initializes a new instance of <see cref="SlimLazy{T, TState}"/> class with the provided factory delegate and state.
-        ///     Optional you can indicate if the initialization should be thread-safe.
         /// </summary>
         /// <param name="valueFactory">
         ///     The value factory delegate.
@@ -160,19 +182,63 @@ namespace System
         /// <param name="state">
         ///     The state.
         /// </param>
-        /// <param name="threadSafe">
-        ///     Indicates if the initialization should be thread-safe.
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when either <paramref name="valueFactory"/> or <paramref name="state"/> are <see langword="null"/>.
+        /// </exception>
+        public SlimLazy(Func<TState, T> valueFactory, TState state)
+        {
+            _valueFactory = Ensure.NotNull(valueFactory);
+            _state = Ensure.NotNull(state);
+        }
+    }
+
+    /// <summary>
+    ///     A lightweight implementation of <see cref="Lazy{T, TMetadata}"/>, which also provides support for lazy initialization.
+    /// </summary>
+    /// <typeparam name="T">
+    ///     The type.
+    /// </typeparam>
+    /// <typeparam name="TState">
+    ///     The state.
+    /// </typeparam>
+    public class ConcurrentSlimLazy<T, TState> : SlimLazy<T, TState> where TState : notnull
+    {
+        private readonly object _lock = new();
+
+        /// <inheritdoc/>
+        public override T Value
+        {
+            get
+            {
+                lock (_lock)
+                    return base.Value;
+            }
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of <see cref="ConcurrentSlimLazy{T, TState}"/> class with the provided value.
+        /// </summary>
+        /// <param name="value">
+        ///     The Value.
+        /// </param>
+        public ConcurrentSlimLazy(T value) : base(value)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of <see cref="ConcurrentSlimLazy{T, TState}"/> class with the provided factory delegate and state.
+        /// </summary>
+        /// <param name="valueFactory">
+        ///     The value factory delegate.
+        /// </param>
+        /// <param name="state">
+        ///     The state.
         /// </param>
         /// <exception cref="ArgumentNullException">
         ///     Thrown when either <paramref name="valueFactory"/> or <paramref name="state"/> are <see langword="null"/>.
         /// </exception>
-        public SlimLazy(Func<TState, T> valueFactory, TState state, bool threadSafe = false)
+        public ConcurrentSlimLazy(Func<TState, T> valueFactory, TState state) : base(valueFactory, state)
         {
-            _valueFactory = Ensure.NotNull(valueFactory);
-            _state = Ensure.NotNull(state);
-
-            if (threadSafe)
-                _lock = new object();
         }
     }
 
