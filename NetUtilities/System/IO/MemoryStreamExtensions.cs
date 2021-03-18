71fe7adf9080a7a420710c0f009e91d7316877e2
AraHaan <seandhunt_7@yahoo.com>
2021-03-18 17:27:53 -0400

    Added MemoryStreamExtensions.cs (#30)
    * Added MemoryStreamExtensions.cs
    
    This will help new people to clear their MemoryStreams a lot easier (possibly before disposing so it is not hard on GC with large MemoryStreams that might be on GC 2 or even LOH).
    
    * Only set capacity when set to 0, or not buffer length.
    
    * Update MemoryStreamExtensions.cs
    
    * Update MemoryStreamExtensions.cs
    
    * Update MemoryStreamExtensions.cs
    
    * Cleanup code a bit.
    
    * Update MemoryStreamExtensions.cs
    
    * Document added parameter, other inline code docs, changed exception being thrown when out of range.
    
    * Update MemoryStreamExtensions.cs

---
+++ NetUtilities/System/IO/MemoryStreamExtensions.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace System.IO
+{
+    /// <summary>
+    /// A extension class for extensions to <see cref="MemoryStream"/>.
+    /// </summary>
+    public static class MemoryStreamExtensions
+    {
+        /// <summary>
+        /// Clears the data in the <see cref="MemoryStream"/>.
+        /// </summary>
+        /// <param name="ms">
+        /// The input <see cref="MemoryStream"/>.
+        /// </param>
+        /// <param name="capacity">
+        /// The capacity to set the capacity of the stream to.
+        /// </param>
+        /// <exception cref="ArgumentNullException">When the input <see cref="MemoryStream"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">When capacity is negative.</exception>
+        public static void Clear(this MemoryStream ms, int capacity)
+        {
+            if (ms == null)
+            {
+                throw new ArgumentNullException(nameof(ms));
+            }
+
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity), "capacity must not be negative.");
+            }
+
+            var len = ms.GetBuffer().Length;
+            var changeCapacity = len != capacity;
+            Array.Clear(ms.GetBuffer(), 0, len);
+            ms.Position = 0;
+            ms.SetLength(0);
+            ms.Capacity = 0; // ensure buffer's bytes are cleared (0 bytes long).
+
+            // avoid setting to the same value twice.
+            if (changeCapacity && capacity != 0)
+            {
+                // resize to passed in capacity to avoid allocating a new array that will
+                // be resized on demand when the user keeps using the stream after clearing
+                // it as it will hurt performance a lot because it will be resized many times.
+                ms.Capacity = capacity;
+            }
+        }
+    }
+}
